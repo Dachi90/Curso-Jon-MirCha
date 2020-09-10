969eaf4c494613fa8f4c930024f840e5ca2d5c37
Martin <bergesio90@gmail.com>
2020-09-10 16:15:53 +0200

    Video 58

--- Video del 57 al 58 this Javascript/script.js
+++ Video del 57 al 58 this Javascript/script.js
@@ -1,6 +1,6 @@
 //          **************************------------------- Video 57 THIS -------------------**************************
 
-console.log(this);
+/* console.log(this);
 console.log(window);
 console.log(this === window);
 
@@ -37,4 +37,40 @@ const obj3 = {
     console.log(this.nombre);
   },
 };
-obj3.imprimir();
+obj3.imprimir(); */
+
+//          **************************------------------- Video 58 call, apply, bind -------------------**************************
+
+console.log(this);
+this.lugar = "Contexto Global";
+// La función saludar está en un contexto global ya que no esta dentro de ningún objeto.
+function saludar(saludo, aQuien) {
+  console.log(`${saludo} ${aQuien} desde el ${this.lugar}`);
+}
+saludar("Hola", "Martín");
+
+const obj = {
+  lugar: "Contexto Objeto obj",
+};
+
+// Con el método .call() le indicamos el objeto el cúal queremos que sea el contexto de this. Por eso ahora nos muestra "Contexto del Objeto obj" en lugar de "contexto global" que sería el de la función saludar.
+saludar.call(obj, "Hola", "Martín");
+
+// Con el metodo .apply() funciona de igual manera con la diferencia que recibe los paremetros en forma de array en lugar de separados por coma com en el método .call()
+saludar.apply(obj, ["Adios", "Martín"]);
+
+// Con el método .bind() podemos asignarle a una funcíon el contexto del objeto que le pasemos como parametro al método bind.
+
+const persona = {
+  nombre: "Martín",
+  saludar: function () {
+    console.log(`Hola ${this.nombre}`);
+  },
+};
+persona.saludar();
+
+const otraPersona = {
+  saludar: persona.saludar.bind(persona),
+};
+
+otraPersona.saludar();
