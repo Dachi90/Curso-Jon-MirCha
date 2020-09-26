081a03c94493aec4a69cea4912497241ec65063c
Martin <bergesio90@gmail.com>
2020-09-26 12:50:35 +0200

    Ejercicio Video 90

--- Video 80 al 104/js/responsive-js.js
+++ Video 80 al 104/js/responsive-js.js
@@ -14,5 +14,6 @@ export default function responsiveMedia(id, mq, mobileContent, desktopContent) {
 
   //breakpoint.addListener(responsive);
   breakpoint.addEventListener("change", responsive);
+
   responsive(breakpoint);
 }
