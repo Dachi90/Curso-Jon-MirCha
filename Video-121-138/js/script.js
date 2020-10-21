490c12b3dab3ce706ed178638b3e85e9f846e2cb
Martin <bergesio90@gmail.com>
2020-10-21 13:51:32 +0200

    Video 122 - Ejercicios Ajax - Solución Jon

--- Video-121-138/js/script.js
+++ Video-121-138/js/script.js
@@ -1,4 +1,5 @@
-let d = document;
+// ************************************** Solución Martín **************************************
+/* let d = document;
 let $html = d.querySelector("html");
 let $body = d.querySelector("body");
 
@@ -43,5 +44,49 @@ d.addEventListener("click", async (e) => {
       let message = err.statusText || "Ocurrio un error";
       $html.innerHTML = `<p><b>Error: ${err.status} ${message}</b></p>`;
     }
+  }
+}); */
+
+//************************************** Solución Jon **************************************
+const d = document,
+  $main = d.querySelector("main");
+
+const getHTML = (options) => {
+  let { url, succes, error } = options;
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let html = xhr.responseText;
+      succes(html);
+    } else {
+      let message = xhr.statusText || "Ocurrió un error";
+      error(`Error ${xhr.status}: ${message}`);
+    }
+  });
+
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Content-type", "text/html; charset=utf-8");
+  xhr.send();
+};
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  getHTML({
+    url: "assets/home.html",
+    succes: (html) => ($main.innerHTML = html),
+    error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
+  });
+});
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".menu a")) {
+    e.preventDefault();
+    getHTML({
+      url: e.target.href,
+      succes: (html) => ($main.innerHTML = html),
+      error: (err) => ($main.innerHTML = `<h1>${err}</h1>`),
+    });
   }
 });
