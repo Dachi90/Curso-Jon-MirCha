0855fce15bedf41e856097742d07ef917ee6da86
Martin <bergesio90@gmail.com>
2020-11-07 13:05:40 +0100

    Video 142 WordPress REST API y Fetch (4 / 5)

--- Video-139-143 Wordpress REST API y Fetch/wp-api-rest.js
+++ Video-139-143 Wordpress REST API y Fetch/wp-api-rest.js
@@ -38,10 +38,44 @@ function getPosts() {
     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
     .then((json) => {
       console.log(json);
+
       json.forEach((el) => {
+        let categories = "",
+          tags = "";
+
+        el._embedded["wp:term"][0].forEach((el) => {
+          categories += `<li>${el.name} </li>`;
+        });
+
+        el._embedded["wp:term"][1].forEach((el) => {
+          tags += `<li>${el.name} </li>`;
+        });
+
         $template.querySelector(".post-image").src = el._embedded["wp:featuredmedia"][0].source_url;
         $template.querySelector(".post-image").alt = el.title.rendered;
+
         $template.querySelector(".post-title").innerHTML = el.title.rendered;
+
+        $template.querySelector(".post-author").innerHTML = `
+        <img src="${el._embedded.author[0].avatar_urls["48"]}" alt="${el._embedded.author[0].name}" >
+        <figcaption> ${el._embedded.author[0].name} </figcaption>
+        `;
+
+        $template.querySelector(".post-date").innerHTML = new Date(el.date).toLocaleDateString();
+        $template.querySelector(".post-link").href = el.link;
+        $template.querySelector(".post-excerpt").innerHTML = el.excerpt.rendered.replace("[&hellip;]", "...");
+
+        $template.querySelector(".post-categories").innerHTML = `
+        <p> Categorías:</p>
+        <ul>${categories}</ul>
+        `;
+
+        $template.querySelector(".post-tags").innerHTML = `
+        <p> Etiquetas:</p>
+        <ul>${tags}</ul>
+        `;
+
+        $template.querySelector(".post-content > article").innerHTML = el.content.rendered;
 
         let $clone = d.importNode($template, true);
         $fragment.appendChild($clone);
