e2886d97186012e9793a6533468661db0f854058
Martin <bergesio90@gmail.com>
2020-11-11 11:52:24 +0100

    Video 145  SPA: Estructura de Proyecto

---
+++ Video-144-148 SPA/app/App.js
@@ -0,0 +1,3 @@
+export function App() {
+  document.getElementById("root").innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla JS</h1>`;
+}
---
+++ Video-144-148 SPA/app/index.js
@@ -0,0 +1,5 @@
+import { App } from "./App.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", App);
