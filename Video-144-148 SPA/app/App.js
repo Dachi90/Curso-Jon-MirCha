10d3cafb5300714e4f9afc9e78e103a861c7f2ef
Martin <bergesio90@gmail.com>
2020-11-12 12:26:30 +0100

    Video 146 SPA: Helper de conexión a API REST

--- Video-144-148 SPA/app/App.js
+++ Video-144-148 SPA/app/App.js
@@ -1,3 +1,7 @@
+import api from "./helpers/wp_api.js";
+
 export function App() {
   document.getElementById("root").innerHTML = `<h1>Bienvenidos a mi primer SPA con Vanilla JS</h1>`;
+
+  console.log(api);
 }
---
+++ Video-144-148 SPA/app/helpers/wp_api.js
@@ -0,0 +1,18 @@
+const NAME = "malvestida",
+  DOMAIN = `https://${NAME}.com`,
+  SITE = `${DOMAIN}/wp-json`,
+  API_WP = `${SITE}/wp/v2`,
+  POSTS = `${API_WP}/posts?_embed`,
+  POST = `${API_WP}/posts`,
+  SEARCH = `${API_WP}/search?_embed&search=`;
+
+// Como el nombre del parametro es igual al nombre del valor se puede simplificar name: NAME a solamente NAME.
+export default {
+  NAME,
+  DOMAIN,
+  SITE,
+  API_WP,
+  POSTS,
+  POST,
+  SEARCH,
+};
